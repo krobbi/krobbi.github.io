52723fb520a334df5ecdf1a3e63990187306904d
krobbi <70552146+krobbi@users.noreply.github.com>
2025-03-28 22:15:29 +0000

    Simplify directory cleaning

--- generate.py
+++ generate.py
@@ -1,24 +1,16 @@
 import os
+import shutil
 
 OUTPUT_DIR: str = "out/"
 """The directory for outputting the site's content."""
 
-def clean_dir(path: str) -> None:
-    """Recursively clean a directory."""
-    
-    for entry in os.scandir(path):
-        if entry.is_dir():
-            clean_dir(entry.path)
-            os.rmdir(entry)
-        else:
-            os.remove(entry)
-
-
 def main() -> None:
-    """Create and clean the output directory."""
+    """Copy static files to the output directory."""
     
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    clean_dir(OUTPUT_DIR)
+    if os.path.exists(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+    
+    shutil.copytree("static/", OUTPUT_DIR)
 
 
 if __name__ == "__main__":
