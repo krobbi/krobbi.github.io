497d047c14a21bbb6cf258b56bf6c4b1f14a2cf5
krobbi <70552146+krobbi@users.noreply.github.com>
2025-03-28 23:04:03 +0000

    Use path objects

--- generate.py
+++ generate.py
@@ -1,16 +1,18 @@
 import os
 import shutil
 
-OUTPUT_DIR: str = "out/"
+from pathlib import Path
+
+OUTPUT_PATH: Path = Path("out/")
 """The directory for outputting the site's content."""
 
 def main() -> None:
     """Copy static files to the output directory."""
     
-    if os.path.exists(OUTPUT_DIR):
-        shutil.rmtree(OUTPUT_DIR)
+    if os.path.exists(OUTPUT_PATH):
+        shutil.rmtree(OUTPUT_PATH)
     
-    shutil.copytree("static/", OUTPUT_DIR)
+    shutil.copytree("static/", OUTPUT_PATH)
 
 
 if __name__ == "__main__":
