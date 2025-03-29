fe2395e82a9656330576d006bbd815ca1bec919b
krobbi <70552146+krobbi@users.noreply.github.com>
2025-03-29 20:49:22 +0000

    Remove undefined tags from rendered templates

--- generator/templates.py
+++ generator/templates.py
@@ -6,21 +6,29 @@ _TAG_PATTERN = re.compile("{\\s*(\\w+)\\s*}")
 class _Template:
     """A text template with tags that can be replaced or appended to."""
     
-    def __init__(self, text: str):
+    def __init__(self, source: str):
         """Initialize the template from its source text."""
         
         # Normalize tags by removing their whitespace.
-        self._text = _TAG_PATTERN.sub("{\\1}", text)
-        """The template's internal text."""
+        self._source = _TAG_PATTERN.sub("{\\1}", source)
+        """The template's internal source text."""
+    
+    
+    def render(self):
+        """Return the template as text with any undefined tags
+        removed.
+        """
+        
+        return _TAG_PATTERN.sub("", self._source)
 
 
 def test():
     """Test the template system."""
     
-    def normalize(text: str) -> str:
+    def normalize(source: str):
         """Return the normalized form of template source text."""
         
-        return _Template(text)._text # type: ignore
+        return _Template(source)._source # type: ignore
     
     # Normalized template text has no surrounding whitespace in tags.
     text = normalize("[{no_spaces}, { spaces }, {\t\r\nmixed_spaces}]")
@@ -33,5 +41,12 @@ def test():
     # Empty or non-alphanumeric names are invalid.
     text = normalize('[{ }, { separating space }, { / }, { "format":"JSON" }]')
     assert text == '[{ }, { separating space }, { / }, { "format":"JSON" }]'
+    
+    # Undefined tags are removed from templates when rendered.
+    template = _Template("Hello, {undefined}!")
+    assert template.render() == "Hello, !"
+    
+    # Templates are not mutated after rendering.
+    assert template._source == "Hello, {undefined}!" # type: ignore
     
     print("Template tests passed!")
